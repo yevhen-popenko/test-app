4156d32db06a4ebb9358010dd69509f680f8b260
Marina Bodak <bodak.marina.ev@gmail.com>
2018-04-09 16:40:48 +0300

    Initial commit. Error in shared module was fixed - ReactiveFormsModule was imported.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from "./form.component";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    // need to import ReactiveFormsModule from @angular/forms here.
+    // formGroup is a selector for directive named FormGroupDirective that is a part of ReactiveFormsModule,
+    // hence the need to import it. It is used to bind an existing FormGroup to a DOM element.
+
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [FormComponent],
   exports: [FormComponent]
