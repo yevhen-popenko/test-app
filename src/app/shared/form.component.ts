c783cc3eadc58ce2df71d3d64e47ff930cd42a31
Le Yu <leyu00@gmail.com>
2018-04-20 16:48:07 -0400

    moved subscriber from constructor to ngOnInit and refactored code to use this.data if properties are populated

--- src/app/shared/form.component.ts
+++ src/app/shared/form.component.ts
@@ -15,10 +15,23 @@ export class FormComponent {
   @Output() sendSubmit: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    let firstName = '';
+    let lastName = '';
+    let age = 0;
+
+    if (this.data) {
+      firstName = this.data.firstName;
+      lastName = this.data.lastName;
+      age = this.data.age;
+    }
+
     this.form = this.fb.group({
-      firstName: ['', [Validators.required]],
-      lastName: ['', Validators.required],
-      age: 0,
+      firstName: [firstName, [Validators.required]],
+      lastName: [lastName, Validators.required],
+      age: age,
     });
   }
 
