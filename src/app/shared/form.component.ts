1dab27fef1326bed64e78fada65a6b992e1c5363
viktornord1 <viktornord1@gmail.com>
2018-05-11 19:52:33 +0300

    fix prefilled data

--- src/app/shared/form.component.ts
+++ src/app/shared/form.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
   form: FormGroup;
   @Input() isEdit = true;
   @Input() error: any;
@@ -13,10 +13,14 @@ export class FormComponent {
   @Output() sendSubmit: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    const preFilledData = this.data || { firstName: '', lastName: '', age: 0 };
     this.form = this.fb.group({
-      firstName: ['', [Validators.required]],
-      lastName: ['', Validators.required],
-      age: 0,
+      firstName: [preFilledData.firstName, [Validators.required]],
+      lastName: [preFilledData.lastName, Validators.required],
+      age: preFilledData.age
     });
   }
 
